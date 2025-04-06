d2a114229b3fe9c87f1827941ae748feb0045da6
Mikhail Yudin <fagci.nsk@gmail.com>
2025-04-06 23:18:23 +0700

    initial

---
+++ work/Proj/Output/crc.d
@@ -0,0 +1,2 @@
+.\output\crc.o: ..\source\Driver\crc.c
+.\output\crc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ work/Proj/Output/startup_bt32f0x.d
@@ -0,0 +1,1 @@
+.\output\startup_bt32f0x.o: ..\source\Libraries\CMSIS\Device\startup_bt32f0x.s
